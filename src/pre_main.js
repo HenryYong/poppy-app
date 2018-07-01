f1e3f2bc6a9934badb80c57fc4cff8535bdf5577
HenryYong <sephenry@gmail.com>
2018-07-02 00:21:00 +0800

    :memo: update file

--- src/pre_main.js
+++ src/pre_main.js
@@ -4,10 +4,10 @@
  */
 
 let script = document.createElement('script')
-
+console.log(NODE_ENV)
 if (NODE_ENV !== 'dev') {
     script.src = 'https://cdn.jsdelivr.net/npm/vue@2.5.16/dist/vue.min.js'
-
+    console.log(script)
     document.body.appendChild(script)
 }
 
